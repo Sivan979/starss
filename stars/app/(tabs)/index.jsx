a1eb04fff988ccc90cbc4b2a923e1dbcaa1c7af8
sivan <sivanhannan7@gmail.com>
2025-03-13 11:55:45 +0100

    update home page

--- stars/app/(tabs)/index.jsx
+++ stars/app/(tabs)/index.jsx
@@ -1,16 +1,31 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import {useRouter} from "expo-router";
+
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View style={{
+      flex: 1,
+      backgroundColor: "#0f0D10",
+    }}
     >
-      <Text>dawg</Text>
+      <Image style={{position: "absolute", width: "100%", zIndex: 0,}} source={images.bg} />
+      <ScrollView style={{flex: 1, padding: 5}} showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10}}>
+        <Image style={{width: 40, marginTop: 80, marginBottom: 20, marginHorizontal: "auto"}} source={icons.logo} />
+
+        <View style={{flex: 1, marginTop: 20,}}>
+          <SearchBar
+            onPress = {() => router.push("/search")}
+            placeholder = "search for a movie"
+          />
+        </View>
+
+      </ScrollView>
     </View>
   );
 }
---
+++ stars/components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+import PropTypes from 'prop-types'
+
+
+
+const SearchBar = ({placeholder, onPress}) => {
+  return (
+    <View style={{flexDirection: "row", alignItems: "center", backgroundColor: "#4f0D29", borderRadius: 50, paddingHorizontal: 20, paddingVertical: 15,}}>
+        <Image style={{width: 20, resizeMode: "contain", tintColor: "#ab8bff"}} source={icons.search} />
+        <TextInput
+            onPress={onPress}
+            placeholder={placeholder}
+            value=''
+            onChangeText={() => {}}
+            placeholderTextColor= "#a8b5db"
+            style={{ flex: 1, marginLeft: 6, color: "white"}}
+        />
+    </View>
+  )
+}
+
+SearchBar.propTypes = {
+    placeholder: PropTypes.string.isRequired,
+    onpress: PropTypes.func,
+};
+
+export default SearchBar
+
+
+const styles = StyleSheet.create({})

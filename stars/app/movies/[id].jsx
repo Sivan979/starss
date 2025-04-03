aefbedc3b7bfe30b40ef2c835a53fa1d0b957d77
sivan <sivanhannan7@gmail.com>
2025-04-03 13:14:06 +0200

    movie info update

--- stars/app/movies/[id].jsx
+++ stars/app/movies/[id].jsx
@@ -1,40 +1,128 @@
-import { View, Text, ScrollView,Image } from 'react-native'
+import { View, Text, ScrollView,Image, TouchableOpacity , StyleSheet} from 'react-native'
 import React from 'react'
-import {useLocalSearchParams} from 'expo-router';
+import {router, useLocalSearchParams} from 'expo-router';
 import useFetch from '../../services/useFetch';
 import {fetchMovieDetails} from '../../services/api';
 import { icons } from '../../constants/icons';
+
+const MovieInfo = ({section, info}) => {
+  return(
+    <View>
+      <Text style={{color: "white", fontWeight:"bold",fontSize:17}}>{section}</Text>
+      <Text style={{color: "#D3D3D3",fontSize:15}}>{info || 'N/A'}</Text>
+    </View>
+  )
+}
 
 const movieDtails = () => {
   const {id} = useLocalSearchParams();
 
   const {data: movie, loading} = useFetch(() => fetchMovieDetails(String(id)))
+  console.log(movie);
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#0f0D10",}}>
+    <View style={styles.container}>
       <ScrollView contentContainerStyle={{ paddingBottom:80}}>
         <View>
-          <Image resizeMode="stretch" style={{width: "100%", height: 600}} source={{uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`}}/>
+          <Image style={styles.mainImg} source={{uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`}}/>
         </View>
 
-        <View style={{flexDirection:"column", alignItems:"flex-start", justifyContent:"center", marginTop:20, paddingHorizontal:20}}>
-          <Text style={{fontWeight:"bold" , color: "white", fontSize:19}}>{movie?.title}</Text>
-        </View>
+        <View style={styles.infoContainer}>
 
-        <View style={{flexDirection:"row", alignItems:"center", marginHorizontal:20, marginTop:8}}>
-          <Text style={{color: "#D3D3D3"}}>{movie?.release_date?.split('-')[0]}</Text>
-          <Text style={{color: "#D3D3D3", marginLeft:10}}>{movie?.runtime}m</Text>
-        </View>
+          <View style={styles.infoUpper}>
+            <Text style={styles.movieTitle}>{movie?.title}</Text>
+            <View style={styles.dateContainer}>
+              <Text style={{color: "#D3D3D3"}}>{movie?.release_date?.split('-')[0]}</Text>
+              <Text style={{color: "#D3D3D3", marginLeft:10}}>{movie?.runtime}m</Text>
+            </View>
+            <View style={styles.voteContainer}>
+              <Image source={icons.star} />
+              <Text style={{color: "#D3D3D3"}}>{Math.round(movie?.vote_average ?? 0)}/10</Text>
+              <Text style={{color: "#D3D3D3", marginLeft:10}}>({movie?.vote_count}) votes</Text>
+            </View>
+          </View>
+          
+          <MovieInfo section="overview" info={movie?.overview}/> 
+          <MovieInfo section="Genres" info={movie?.genres?.map((g) => g.name).join(' - ') || 'N/A'}/>
+          <View style={styles.budgetContainer}>
+            <MovieInfo section="Budget" info={`$${movie?.budget / 1000000} million`}/> 
+            <MovieInfo section="Revenue" info={`$${Math.round(movie?.revenue / 1000000)} million`}/> 
+          </View>
+          <MovieInfo section="production_companies" info={movie?.production_companies?.map((g) => g.name).join(' - ') || 'N/A'}/> 
 
-        <View style={{flexDirection:"row", alignItems:"center", backgroundColor:"blue", paddingHorizontal:4, paddingVertical:2, borderRadius:30, marginTop:5, width:"35%"}}>
-          <Image source={icons.star} />
-          <Text style={{color: "#D3D3D3"}}>{Math.round(movie?.vote_average ?? 0)}/10</Text>
-          <Text style={{color: "#D3D3D3", marginLeft:10}}>({movie?.vote_count}) votes</Text>
         </View>
-
       </ScrollView>
+
+      <TouchableOpacity style={styles.backBtn} onPress={router.back}>
+        <Image style={{transform: [{ rotate: '180deg' }], }} source={icons.arrow} />
+        <Text style={styles.backBtnText}>Back</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "#0f0D10",
+  },
+  mainImg:{
+    width: "100%",
+    height: 600,
+    resizeMode:"stretch",
+  },
+  infoContainer:{
+    flexDirection:"column",
+    alignItems:"flex-start",
+    justifyContent:"center",
+    marginTop:20, 
+    paddingHorizontal:20,
+    rowGap:25,
+
+  },
+  infoUpper:{
+    rowGap: 5,
+  },
+  movieTitle:{
+    fontWeight:"bold",
+    color: "white",
+    fontSize:20,
+    marginBottom:15,
+  },
+  dateContainer:{
+    flexDirection:"row",
+    alignItems:"center",
+  },
+  voteContainer:{
+    flexDirection:"row",
+    alignItems:"center",
+    backgroundColor:"blue",
+    paddingHorizontal:3,
+    width:"100%",
+    paddingVertical:2,
+    borderRadius:8,
+  },
+  budgetContainer:{
+    flexDirection:"row",
+    columnGap:70
+  },
+  backBtn:{
+    backgroundColor:"white",
+    width:"100%",
+    height:50,
+    borderRadius: 50,
+    marginBottom:40,
+    alignItems:"center",
+    justifyContent:"center",
+    flexDirection:"row",
+    gap:2,
+  },
+  backBtnText:{
+    color:"red",
+    fontWeight:"bold",
+    fontSize: 18,
+    letterSpacing:0.5,
+  }
+});
 
 export default movieDtails
